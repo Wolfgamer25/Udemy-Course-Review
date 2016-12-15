6e6d0abcb91d55ec7855bb4d1b4f76b2faf54da6
Jonathan Torres <jonathantorres@Jonathans-MacBook-Pro.local>
2016-12-15 16:26:12 -0500

    Updated DB connection code to use MongoLab when in Production

--- app.js
+++ app.js
@@ -16,7 +16,22 @@ var commentRoutes    = require("./routes/comments"),
     udemyRoutes = require("./routes/udemy"),
     indexRoutes      = require("./routes/index")
 
-mongoose.connect("mongodb://localhost/UdemyReview");
+    // Connect to database
+    if (process.env.MONGODB_URI) {
+      mongoose.connect(process.env.MONGODB_URI);
+    }
+    else {
+      mongoose.connect('mongodb://localhost/UdemyReview');
+    }
+    mongoose.connection.on('error', function(err) {
+      console.error('MongoDB connection error: ' + err);
+      process.exit(-1);
+      }
+    );
+    mongoose.connection.once('open', function() {
+      console.log("Mongoose has connected to MongoDB!");
+    });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
